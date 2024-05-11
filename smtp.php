56a7864953010e36d3800fe0452548ded109d8d2
Vitor Carvalho <lightningspirit@gmail.com>
2024-05-11 10:14:39 +0100

    fix(api): match slug

--- smtp.php
+++ smtp.php
@@ -9,7 +9,7 @@
  * Author URI:      https://moveyourdigital.com
  * License:         GPLv2
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:      https://raw.githubusercontent.com/moveyourdigital/wp-smtp/main/wp-info.json
+ * Update URI:      https://github.com/moveyourdigital/wp-smtp/raw/main/wp-info.json
  * Text Domain:     smtp
  * Domain Path:     /languages
  *
